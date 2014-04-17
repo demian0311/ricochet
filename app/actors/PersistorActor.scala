82693263cb2c6358b6c9aa9a9565986d447feaf6
Demian Neidetcher <demian0311@gmail.com>
2014-04-16 21:17:17 -0500

    synchronize access to mutable map

--- app/actors/PersistorActor.scala
+++ app/actors/PersistorActor.scala
@@ -1,7 +1,7 @@
 package actors
 
 import akka.actor.Actor
-import utils.{TimerEventRequest, TimerEventPost, TimerEvent}
+import utils.{GaugePersistence, TimerEventRequest, TimerEventPost, TimerEvent}
 import play.api.mvc.{Results, Result}
 import scala.collection.mutable
 import play.api.libs.json.{Writes, Json, JsValue}
@@ -9,27 +9,28 @@ import play.api.libs.json.{Writes, Json, JsValue}
 class PersistorActor extends Actor with Results {
 
   // And, now the gauge needs to come out :)
-  val gauge = collection.mutable.Map[String, collection.mutable.MutableList[TimerEvent]]()
+  //val gauge = collection.mutable.Map[String, collection.mutable.MutableList[TimerEvent]]()
 
-  implicit val TimerEventWrites = new Writes[TimerEvent] {
-    def writes(timerEvent: TimerEvent) = Json.obj(
-      "duration" -> timerEvent.duration,
-      "dateTime" -> timerEvent.dateTime
-    )
-  }
+//  implicit val TimerEventWrites = new Writes[TimerEvent] {
+//    def writes(timerEvent: TimerEvent) = Json.obj(
+//      "duration" -> timerEvent.duration,
+//      "dateTime" -> timerEvent.dateTime
+//    )
+//  }
 
   def receive = {
     case TimerEventPost(path, TimerEvent(duration, dateTime)) => {
-      persist(TimerEventPost(path, TimerEvent(duration, dateTime)))
+      GaugePersistence.persist(TimerEventPost(path, TimerEvent(duration, dateTime)))
     }
     case TimerEventRequest(path) => {
-      sender.forward(report(TimerEventRequest(path)))
+      sender.forward(GaugePersistence.report(TimerEventRequest(path)))
     }
     case _ => {
       println("don't know how to handle")
     }
   }
 
+  /*
   def persist(timerEventPost: TimerEventPost) {
     Thread.sleep(500)
 
@@ -38,7 +39,9 @@ class PersistorActor extends Actor with Results {
       case None => gauge += (timerEventPost.path -> mutable.MutableList(timerEventPost.timerEvent))
     }
   }
+  */
 
+  /*
   def report(timerEventRequest: TimerEventRequest): Result = {
     Thread.sleep(500)
 
@@ -57,4 +60,5 @@ class PersistorActor extends Actor with Results {
       case None => NotFound("couldn't find gauge for: " + timerEventRequest.path)
     }
   }
+*/
 }
--- app/utils/Model.scala
+++ app/utils/Model.scala
@@ -1,7 +1,56 @@
 package utils
 
 import org.joda.time.DateTime
+import scala.collection.mutable
+import play.api.mvc.{Results, Result}
+import play.api.libs.json.{Writes, Json, JsValue}
 
 case class TimerEventPost(path: String, timerEvent: TimerEvent)
 case class TimerEvent(duration: Int, dateTime: DateTime = DateTime.now())
 case class TimerEventRequest(path: String)
+
+object GaugePersistence extends Results {
+
+  implicit val TimerEventWrites = new Writes[TimerEvent] {
+    def writes(timerEvent: TimerEvent) = Json.obj(
+      "duration" -> timerEvent.duration,
+      "dateTime" -> timerEvent.dateTime
+    )
+  }
+
+  val mapOfTimerEvents = collection.mutable.Map[ String,  collection.mutable.MutableList[TimerEvent]]()
+  //val mapOfTimerEvents = Map[ String,  collection.mutable.MutableList[TimerEvent]]()
+
+  def persist(timerEventPost: TimerEventPost) {
+    Thread.sleep(500)
+
+    this.synchronized {
+      mapOfTimerEvents.get(timerEventPost.path) match {
+        case Some(gaugeList) => gaugeList += timerEventPost.timerEvent
+        case None => mapOfTimerEvents += (timerEventPost.path -> mutable.MutableList(timerEventPost.timerEvent))
+      }
+    }
+  }
+
+  def report(timerEventRequest: TimerEventRequest): Result = {
+    Thread.sleep(500)
+
+    mapOfTimerEvents.get(timerEventRequest.path) match {
+      case Some(listData) => {
+        val jsonOut: JsValue = Json.obj(
+          "path" -> timerEventRequest.path,
+          "count" -> listData.size,
+          "max" -> listData.map(_.duration).max,
+          "min" -> listData.map(_.duration).min,
+          "timerEvents" -> Json.toJson(listData)
+        )
+
+        Ok(jsonOut)
+      }
+      case None => NotFound("couldn't find gauge for: " + timerEventRequest.path)
+    }
+  }
+
+
+
+}
